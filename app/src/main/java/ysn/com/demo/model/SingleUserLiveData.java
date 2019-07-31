484412b3416a75e908049d417f6f0db897203277
杨三宁 <ysn_2020@163.com>
2019-07-31 10:12:15 +0800

    [ +] 增加LiveData方法说明

--- app/src/main/java/ysn/com/demo/model/SingleUserLiveData.java
+++ app/src/main/java/ysn/com/demo/model/SingleUserLiveData.java
@@ -7,7 +7,7 @@ import ysn.com.demo.bean.User;
 /**
  * @Author yangsanning
  * @ClassName SingleUserLiveData
- * @Description 一句话概括作用
+ * @Description 单例
  * @Date 2019/7/30
  * @History 2019/7/30 author: description:
  */
@@ -15,6 +15,9 @@ public class SingleUserLiveData extends LiveData<User> {
 
     private static SingleUserLiveData instance;
 
+    /**
+     * 采用单例的目的: 让多个Activity或者Fragment共享一个实例
+     */
     public static synchronized SingleUserLiveData get() {
         if (instance == null) {
             synchronized (SingleUserLiveData.class) {
@@ -26,13 +29,33 @@ public class SingleUserLiveData extends LiveData<User> {
         return instance;
     }
 
+    /**
+     * 子线程调用post
+     */
     @Override
     public void postValue(User value) {
         super.postValue(value);
     }
 
+    /**
+     * 更新LiveData数据，并通知处于活动状态的观察者
+     */
     @Override
     public void setValue(User value) {
         super.setValue(value);
+    }
+
+    /**
+     * 观察者数量从0变为了1时调用
+     */
+    @Override
+    protected void onActive() {
+    }
+
+    /**
+     * 观察者数量变为了0时调用
+     */
+    @Override
+    protected void onInactive() {
     }
 }
